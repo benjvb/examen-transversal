8ac21037d36aac9433af10978761cf443b9f1a6b
benjvb <be.pizarrou@duocuc.cl>
2025-07-10 21:46:04 -0400

    Add files via upload

---
+++ examen transversal BPU.py
@@ -0,0 +1,52 @@
+|
+stock = {'HP', 15.6, '8GB', 'DD', '1T', 'Intel Core i3', 'integrada'
+         'Asus', 14, '16GB', 'SSD', '256GB', 'Intel Core i7', }
+
+
+
+def mostrar_menu():
+    print ("/n Menu pricipal")
+    print ("1.stock por marca ")
+    print ("2.busqueda de precio")
+    print ("3.actualizacion de precio")
+    print ("4.salir del programa ")
+
+def mostrar_stock():
+    print ("/n  stock por marca ")
+    for marcaa.precio stock items():
+    print(f"{marca} ") (f"{precios} ")
+    
+def busqueda_precio():
+    try:
+     float(input("ingrese el precio"))
+    print ("/resultados")
+    for marcaa.precio stock items():
+    if precio in precio:
+    print(f"{marca }tiene estos computadores{ precio} ")
+    encontrado=True
+    if not encontrado:
+    except ValueError:
+    print ("por favor ,ingrese un numero valido")
+
+def actualizar_precio():
+    int=input("ingrese  la marca del computador ")
+    precio_viejo = float(input("ingrese el precio actual")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
